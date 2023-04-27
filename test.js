df9c61b2c867f07b9477eca5c465ba166fee23de
VIshal Kumar <76954149+vishalkumar-vk@users.noreply.github.com>
2023-04-27 12:37:10 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 // Requiring module
 const assert = require('assert');
 //sample comment for jenkins build
-//sample comment
 // We can group similar tests inside a describe block
 describe("Simple Calculations", () => {
 before(() => {
